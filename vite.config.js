c0664d25e25fe7f98c3929498be1e31a2a8bf4f0
miloezger <milo.peter@deuxsign.ch>
2025-05-23 16:23:30 +0200

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,5 @@
 export default {
   server: {
     open: true
-  },
-  preview: {
-    open: true
-  },
-  build: {
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  },
-  publicDir: 'public'
+  }
 }
