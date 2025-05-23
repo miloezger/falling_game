56f1126b6950f2188402b19fc239f52b46d9e723
miloezger <milo.peter@deuxsign.ch>
2025-05-23 16:09:45 +0200

    Configure Vite to handle image assets

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 export default {
   server: {
     open: true
-  }
+  },
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: 'index.html'
+      }
+    }
+  },
+  publicDir: 'images'
 }
