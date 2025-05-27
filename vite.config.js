794c76896c15f8be49e701e034890a069d1ec148
miloezger <milo.peter@deuxsign.ch>
2025-05-27 21:56:46 +0200

    Deploy application to Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,5 @@
 export default {
   server: {
     open: true
-  },
-  build: {
-    assetsInlineLimit: 0, // Don't inline any assets
-    rollupOptions: {
-      input: {
-        main: 'index.html'
-      },
-      output: {
-        assetFileNames: (assetInfo) => {
-          // Keep the original directory structure for media files
-          if (assetInfo.name.includes('media/')) {
-            return '[name][extname]';
-          }
-          return 'assets/[name]-[hash][extname]';
-        }
-      }
-    }
   }
 }
